00dd00f308f5ca8843c73f14105ad9ee3721296e
Raj <raj.m.rao@gmail.com>
2018-05-14 09:01:10 -0600

    Updated code to add tracelevel

--- BIN/CrmWebApiAccessor.pm
+++ BIN/CrmWebApiAccessor.pm
@@ -24,13 +24,15 @@ sub new {
 	my $this = {
 		debug => 1,
 		expiresAt => DateTime->now->subtract(seconds => 1),
-		token => ""
+		token => "",
+		traceLevel => 1,  #1 - verbose, #2 -debug #3-important messages 
 	};
 
 	$this->{crmUrl} = $parm{'crmUrl'};
 	$this->{crmApiUrl} = $parm{'crmApiUrl'};
 	$this->{clientId} = $parm{'clientId'};
 	$this->{clientSecret} = $parm{'clientSecret'};
+	$this->{traceLevel} = $parm{'traceLevel'};
 	
 	if (defined $parm{'debug'})
 	{
@@ -38,113 +40,129 @@ sub new {
 		
 		if ($this->{debug} == 1)
 		{
+			print 'CrmWebApiAccessor instance data:' . "\n";
 			print 'crmUrl:' . $this->{crmUrl} . "\n";
 			print 'crmApiUrl:' .$this->{crmApiUrl} . "\n";
 			print 'clientId:' . $this->{clientId} . "\n";
 			print 'clientSecret:' . $this->{clientSecret} . "\n";
+			print 'traceLevel:' . $this->{traceLevel} . "\n\n";
 		}
 	}
 	
 	bless $this, $type;		
 }
 
-sub getFirstName {
-	my($this) = @_;
-    return $this->{'crmUrl'};
-}
 
 sub debugPrint {
-	my($msg) = @_;
-    # if (defined $this->{debug})
-	# {
-	print $msg . "\n";
-	# }
+	my($this, $msg, $traceLevel) = @_;
+    if (defined $this->{debug} && $traceLevel >= $this->{traceLevel})
+	{
+		print $msg . "\n";
+	}
 }
 
 sub getAuthorizationUri{
 	my ($this) = @_;
-	print 'getting auth uri....' . "\n";
+	debugPrint($this, 'getting auth uri....' . "\n", 2);
 	my $header = ['Content-Type' => 'application/json; charset=UTF-8'];
 	my $request = HTTP::Request->new(Get => $this->{crmApiUrl}, $header);
 	my $ua=LWP::UserAgent->new();
 	
-	my $response = $ua->request($request);
-	
-	my $wwwAuthenticateHeader = $response->header('www-authenticate');
 	my $authuri = "";
 	my $resourceUri = "";
 	
-	($authuri, $resourceUri) = $wwwAuthenticateHeader =~ m/authorization_uri=(?<u>.*authorize).* resource_id=(?<r>.*)/g;
-	
-	$authuri =~ s/authorize/token/g;
-	
-	return ($authuri, $resourceUri);
-}
-
-sub getToken{
-	my ($this) = @_;
-	print 'getting token....' . "\n";
-	my ($authUri, $resourceUri) = getAuthorizationUri($this);
-	#debugPrint ("Authorization Uri: " . $authUri);
-	
-	my $header = ['Content-Type' => 'application/x-www-form-urlencoded;'];
-	my $content = "resource=$resourceUri&client_id=" . $this->{clientId} . "&client_secret=" . $this->{clientSecret} . "&grant_type=client_credentials";
-	my $request = HTTP::Request->new(Post => $authUri, $header,$content);
-		
-	my $ua=LWP::UserAgent->new();
-	
 	my $response = $ua->request($request);
-	#debugPrint ($response->content);
-	my $token = "";
-	my $expiresIn = DateTime->now;
-	if ($response->is_success)
+	if ($response->code == 401)
 	{
-		my $json = decode_json($response->content);
-		#debugPrint(Dumper($json));
+		my $wwwAuthenticateHeader = $response->header('www-authenticate');
+		($authuri, $resourceUri) = $wwwAuthenticateHeader =~ m/authorization_uri=(?<u>.*authorize).* resource_id=(?<r>.*)/g;
 		
-		$token = $json->{'access_token'};
-		$expiresIn->add(seconds => $json->{'expires_in'});
+		$authuri =~ s/authorize/token/g;
+		
+		debugPrint ($this, "Authorization Uri: " . $authuri . "Resource Uri: " . $resourceUri, 2);
 	}
-	else {
-		print "Error retrieving token:\n";
+	else
+	{
+		print "Error retrieving auth uri. Expected code 401\n";
 		print "Code was ", $response->code, "\n";
 		print "Msg: ", $response->message, "\n";
 		print $response->content, "\n";
 		die;
 	}
-	$this->{token}  = $token;
-	$this->{expiresAt} = $expiresIn;
+	return ($authuri, $resourceUri);
+}
+
+sub getToken{
+	my ($this) = @_;
+	debugPrint($this, 'getting token....' . "\n", 2);
+	my ($authUri, $resourceUri) = getAuthorizationUri($this);
+		
+	my $token = "";
+	my $expiresIn = DateTime->now;
+	
+	if ($authUri ne "")
+	{
+		my $header = ['Content-Type' => 'application/x-www-form-urlencoded;'];
+		my $content = "resource=$resourceUri&client_id=" . $this->{clientId} . "&client_secret=" . $this->{clientSecret} . "&grant_type=client_credentials";
+		my $request = HTTP::Request->new(Post => $authUri, $header,$content);
+			
+		my $ua=LWP::UserAgent->new();
+		
+		my $response = $ua->request($request);
+		debugPrint ($this, $response->content, 1);
+		
+		if ($response->is_success)
+		{
+			my $json = decode_json($response->content);
+			debugPrint($this, Dumper($json), 1);
+			
+			$token = $json->{'access_token'};
+			$expiresIn->add(seconds => $json->{'expires_in'});
+			
+			$this->{token}  = $token;
+			$this->{expiresAt} = $expiresIn;
+		
+			debugPrint($this, 'token retrieved. Expires at: ' . $expiresIn . "\n", 2);
+		}
+		else {
+			print "Error retrieving token:\n";
+			print "Code was ", $response->code, "\n";
+			print "Msg: ", $response->message, "\n";
+			print $response->content, "\n";
+			die;
+		}		
+	}
+		
 	return ($token, $expiresIn);
 }
 
 sub getData{
 	my ($this, $query)= @_;
-	print 'getting data....' . "\n";
+	print 'getting data for query: ' . $query . "\n";
 	my $queryUri = $this->{crmApiUrl} . $query;
-	debugPrint($queryUri);
+	debugPrint($this, $queryUri, 3);
 	
 	if (DateTime->now > $this->{expiresAt})
 	{
-		print 'token expired, getting new token' . "\n";
-		
+		debugPrint($this, 'token expired, getting new token' . "\n", 3);
 		getToken($this);
-		
-		print $this->{token} . "\n";
 	}
-	print 'getting data....' . "\n";
+	
+	debugPrint($this, 'sending request to get data.... ' . "\n", 2);
+	
 	my $crmApiUa = LWP::UserAgent->new();
 	$crmApiUa->default_header(Authorization => 'Bearer ' . $this->{token});
 	my $apiResponse = $crmApiUa->get($queryUri);
 	
-	print 'response received!' . "\n";
-	
+	debugPrint($this, 'response received!' . "\n", 2);
+		
 	my $apiData;
 	if($apiResponse->is_success) {
 		my $jsonData = decode_json($apiResponse->content);
-		#debugPrint(Dumper($jsonData->{'value'}));
+		debugPrint($this, Dumper($jsonData->{'value'}), 1);
 		$apiData = $jsonData->{'value'};
-		debugPrint ("type: " . (ref $apiData));
-		#debugPrint(Dumper($apiData));
+		debugPrint ($this, "type: " . (ref $apiData), 1);
+		debugPrint($this, Dumper($apiData), 1);
 	} 
 	else {
 		print "Error retrieving data:\n";
@@ -154,8 +172,8 @@ sub getData{
 		die;
 	}
 	
-	print 'done getting data! retrieved: ' . scalar(@$apiData) . " elements! \n";
-	
+	debugPrint($this, 'done getting data! retrieved: ' . scalar(@$apiData) . " elements! \n", 3);
+		
 	return @$apiData;
 }
 
--- crmSample.pl
+++ crmSample.pl
@@ -2,6 +2,12 @@ use FindBin;
 use lib $FindBin::Bin;
 use Bin::CrmWebApiAccessor;
 
+use Win32::Console;
+
+my $OUT = Win32::Console->new(STD_OUTPUT_HANDLE);
+my $clear_string = $OUT->Cls;
+print $clear_string;
+
 
 my $crmUrl = "https://xxxxxxxx.crm.dynamics.com/";
 my $crmApiUrl = $crmUrl . "api/data/V8.2/";
@@ -9,7 +15,7 @@ my $crmApiUrl = $crmUrl . "api/data/V8.2/";
 my $clientId="00000000-0000-0000-0000-000000000000";
 my $clientSecret="yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy";
 
-my $c = new CrmWebApiAccessor('crmUrl' => $crmUrl, 'crmApiUrl' => $crmApiUrl, 'clientId' => $clientId,  'clientSecret' => $clientSecret, 'debug' => 1);
+my $c = new CrmWebApiAccessor('crmUrl' => $crmUrl, 'crmApiUrl' => $crmApiUrl, 'clientId' => $clientId,  'clientSecret' => $clientSecret, 'debug' => 1, 'traceLevel' => 2);
 
 my (@data) = $c->getData('systemusers?$top=2&$select=fullname');
 
@@ -34,6 +40,6 @@ for my $item( @data ){
 	print $item->{"fullname"} . " {" . $item->{"systemuserid"} ."}". "\n";
 };
 
-my (@data) = $c->getData('accounts?$select=accountid,name&$top=20');
+my (@data) = $c->getData('accounts?$select=accountid,name&$top=2');
 
 $c->printData(@data);
